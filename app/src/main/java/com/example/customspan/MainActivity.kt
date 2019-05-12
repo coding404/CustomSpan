4c303c6d41f8f9a0add7da6652fc77204dad7956
coding404 <liushu@liushudeMacBook-Pro.local>
2019-05-12 18:07:05 +0800

    test

--- app/src/main/java/com/example/customspan/MainActivity.kt
+++ app/src/main/java/com/example/customspan/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
 
         val content="测试一下<span style='font-size:24px'>设置内字体大小为24px</span>,颜色为<span style='color:red'>红色</span>字号 "
         MyUtils.useHtml(tv_content,content)
+
     }
 }
